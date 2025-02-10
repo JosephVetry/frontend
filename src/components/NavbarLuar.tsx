f4f4d4e4ab022e69458dc508045e5568daa41512
Joseph Vetry <josefvetry974@gmail.com>
2025-02-10 16:04:20 +0700

    change icon

--- src/components/NavbarLuar.tsx
+++ src/components/NavbarLuar.tsx
@@ -7,7 +7,7 @@ export default function Component() {
   return (
     <Navbar fluid rounded className="fixed top-0 left-0 w-full z-50 bg-white shadow-md">
         <Navbar.Brand href="https://google.com">
-        <img src="https://logos-world.net/wp-content/uploads/2023/07/Las-Vegas-Logo.png" 
+        <img src="https://png.pngtree.com/png-vector/20220623/ourmid/pngtree-opened-capsule-icon-logo-cartoon-png-image_5319391.png" 
             className="mr-3 h-6 sm:h-9" 
             alt="Flowbite React Logo" />
         <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
