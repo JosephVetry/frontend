eecc09e2c9cb90fc51ef9d2ab9a9b8fc7e4e2ea7
Joseph Vetry <josefvetry974@gmail.com>
2025-02-10 16:14:54 +0700

    test2

--- src/components/NavbarLuar.tsx
+++ src/components/NavbarLuar.tsx
@@ -6,8 +6,8 @@ import { Navbar } from "flowbite-react";
 export default function Component() {
   return (
     <Navbar fluid rounded className="fixed top-0 left-0 w-full z-50 bg-white shadow-md">
-        <Navbar.Brand href="https://google.com">
-        <img src="https://logos-world.net/wp-content/uploads/2023/07/Las-Vegas-Logo.png" 
+        <Navbar.Brand href="https://wpharmacy.netlify.app/">
+        <img src="https://png.pngtree.com/png-vector/20220623/ourmid/pngtree-opened-capsule-icon-logo-cartoon-png-image_5319391.png" 
             className="mr-3 h-6 sm:h-9" 
             alt="Flowbite React Logo" />
         <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
