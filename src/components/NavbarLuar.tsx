18600795ede49527c77b9d9f85d9da1dbeeb8473
Joseph Vetry <josefvetry974@gmail.com>
2025-02-10 16:05:59 +0700

    changes

--- src/components/NavbarLuar.tsx
+++ src/components/NavbarLuar.tsx
@@ -6,7 +6,7 @@ import { Navbar } from "flowbite-react";
 export default function Component() {
   return (
     <Navbar fluid rounded className="fixed top-0 left-0 w-full z-50 bg-white shadow-md">
-        <Navbar.Brand href="https://google.com">
+        <Navbar.Brand>
         <img src="https://png.pngtree.com/png-vector/20220623/ourmid/pngtree-opened-capsule-icon-logo-cartoon-png-image_5319391.png" 
             className="mr-3 h-6 sm:h-9" 
             alt="Flowbite React Logo" />
